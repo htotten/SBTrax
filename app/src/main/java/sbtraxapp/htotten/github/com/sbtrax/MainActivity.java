937a30a56ec34c212dd1dddd6c7b1759a225126b
Heather Totten <htotten@westmont.edu>
2019-01-26 13:30:02 -0800

    added second button functionality

--- app/src/main/java/sbtraxapp/htotten/github/com/sbtrax/MainActivity.java
+++ app/src/main/java/sbtraxapp/htotten/github/com/sbtrax/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     private ImageButton trail1;
+    private ImageButton trail2;
 
 
 
@@ -20,9 +21,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         trail1 = findViewById(R.id.trail1button);
+        trail2 = findViewById(R.id.trail2button);
+
         trail1.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, Trail1Activity.class));
+            }
+        });
+        trail2.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Trail2Activity.class));
             }
         });
     }

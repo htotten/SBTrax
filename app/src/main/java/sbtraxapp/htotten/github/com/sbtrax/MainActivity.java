9156cc31ab6f4af386c183fffff615df69f9429f
Heather Totten <htotten@westmont.edu>
2019-01-26 13:19:51 -0800

    first commit

---
+++ app/src/main/java/sbtraxapp/htotten/github/com/sbtrax/MainActivity.java
@@ -0,0 +1,41 @@
+package sbtraxapp.htotten.github.com.sbtrax;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageButton;
+import android.view.View.OnClickListener;
+import android.view.View;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private ImageButton trail1;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        trail1 = findViewById(R.id.trail1button);
+        trail1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Trail1Activity.class));
+            }
+        });
+    }
+
+
+
+
+/*
+    OnClickListener captureListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+
+        }
+    };*/
+}

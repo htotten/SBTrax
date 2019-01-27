5f52377965ad6fe588371ed56481ae8b6ad6619f
Sophia <sgigliotti@westmont.edu>
2019-01-26 22:50:35 -0800

    button working almost, need start again functionality

--- app/src/main/java/sbtraxapp/htotten/github/com/sbtrax/EmergenciesActivity.java
+++ app/src/main/java/sbtraxapp/htotten/github/com/sbtrax/EmergenciesActivity.java
@@ -1,13 +1,41 @@
 package sbtraxapp.htotten.github.com.sbtrax;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class EmergenciesActivity extends AppCompatActivity {
+    private FloatingActionButton btn;
+    private boolean clicked;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergencies);
+
+        btn = (FloatingActionButton) findViewById(R.id.emergencyButton);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.alarm2);
+
+
+        btn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                if(!clicked){
+                    mp.start();
+                    clicked = true;
+                }
+                else{
+                    mp.stop();
+                    clicked = false;
+                }
+
+            }
+        });
+
+
     }
 }
